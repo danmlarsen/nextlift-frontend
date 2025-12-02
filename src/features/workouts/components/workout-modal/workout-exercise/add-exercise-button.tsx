662e80f49a8238b8361d7c428586d73e16905687
Dan Marius Larsen <larsendanmarius@gmail.com>
2025-12-02 16:37:14 +0100

    refactor: remove unused useState import from AddExerciseButton component

--- src/features/workouts/components/workout-modal/workout-exercise/add-exercise-button.tsx
+++ src/features/workouts/components/workout-modal/workout-exercise/add-exercise-button.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import { useAddWorkoutExercise } from "@/api/workouts/workout-exercise-mutations";
 import ExercisesView from "@/features/exercises/components/exercises-view/exercises-view";
